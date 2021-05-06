a4bb8bfebec25d03ddc82ff8a28a5dda09077ea1
serjunfan <80159533+serjunfan@users.noreply.github.com>
2021-05-06 23:41:27 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useData } from './useData';
+import { Marks } from './Marks';
+import { immiData } from './immiData';
+import { getlalo } from './getlalo';
+import { setuppoints } from './setuppoints';
+
+const width = 960;
+const height = 500;
+//const projection = d3.geoNaturalEarth1();
+
+const App = () => {
+  const graphdata = useData();
+  const immidata = immiData();
+  if (!graphdata || !immidata ) {
+    return <pre>Loading...</pre>;
+  }
+  //console.log(graphdata);
+  const countrytable = new Map();
+  graphdata.countries.features.forEach( d =>{
+   countrytable.set(d.properties.name
+    ,(getlalo(d.geometry.coordinates)));
+  })
+  setuppoints(countrytable,immidata);
+  console.log(immidata);
+
+  return (
+    <svg width={width} height={height}>
+      <Marks graphdata={graphdata} immidata={immidata}/>
+    </svg>
+  );
+};
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
