96f5ed2c31adcb5a2358a74ec2eea8bc4e1fa2c0
serjunfan <80159533+serjunfan@users.noreply.github.com>
2021-05-06 23:41:41 +0800

    Create Marks.js

---
+++ Marks.js
@@ -0,0 +1,27 @@
+import { geoNaturalEarth1, geoPath, geoGraticule } from 'd3';
+
+const projection = geoNaturalEarth1();
+const path = geoPath(projection);
+const graticule = geoGraticule();
+let link =[ 
+  {type: "LineString", coordinates: [[100, 60], [-60, -30]],abc:"abc"},
+  {type: "LineString", coordinates: [[10, -20], [-60, -30]]},
+  {type: "LineString", coordinates: [[10, -20], [130, -30]]}]
+
+
+
+export const Marks = ( {graphdata: { countries, interiors }
+  ,immidata }) => (
+  <g className="marks">
+    <path className="sphere" d={path({ type: 'Sphere' })} />
+    <path className="graticules" d={path(graticule())} />
+    {countries.features.map(feature => (
+      <path className="country" d={path(feature)} />
+    ))}
+    <path className="interiors" d={path(interiors)} />
+    {immidata.map( d => (
+    <path className="flows" d={path(d)} />
+    )) 
+     }
+  </g>
+);
