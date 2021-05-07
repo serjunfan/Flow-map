a7e4b200fcaebd6c24877ec6e98b35fdef151bfd
serjunfan <80159533+serjunfan@users.noreply.github.com>
2021-05-07 09:46:53 +0800

    Update getlalo.js
    Two Graph data coordinates are different than others so i tweaked a little bit code to fix the bug of NaN

--- getlalo.js
+++ getlalo.js
@@ -1,9 +1,10 @@
 //A function that gets the center point Latitude and  Longitude by Summing all the points Latitude and  Longitude and divide them by how many points.
-export const getlalo = a =>{
+//Two Graph data coordinates are different than others so i tweaked a little bit code to fix the bug of NaN 
+export const getlalo = (a,index) =>{
   let lacoorsum = 0;
   let locoorsum = 0; 
   let totalcoordinates = a.length
-  if( totalcoordinates == 1){
+  if( totalcoordinates == 1 || index == 102 || index == 128){
   a.forEach( d => {
     let totalpoints = d.length;
     let lacoor = 0;
