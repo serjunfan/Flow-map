8ee595227b8e32b2abafb502eac3e0754bc23566
serjunfan <80159533+serjunfan@users.noreply.github.com>
2021-06-09 15:53:38 +0800

    Update and rename immiData.js to immiData1.js

--- immiData1.js
+++ immiData1.js
@@ -2,8 +2,9 @@
 import { useState, useEffect } from 'react';
 import { csv } from 'd3';
 
-const dataUrl ="https://gist.githubusercontent.com/serjunfan/b3f0b16e32c153b8393338bf997bdc08/raw/4a0b8e022f09de28bfae808da66bed1b311c61e7/2020.csv";
-export const immiData = () => {
+
+const dataUrl ="https://gist.githubusercontent.com/serjunfan/11f2e26371afcb897fdbed8ac26ff5bf/raw/ec9a0d9b4e6c5dbce4a65eb1cf7730f868c9704f/2020_Russia_ver1.csv";
+export const immiData1 = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
     csv(dataUrl).then(setData);
