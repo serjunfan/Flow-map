1b9b4d483a3116dd0e967b3acf76aa24c5d9a9db
serjunfan <80159533+serjunfan@users.noreply.github.com>
2021-06-09 15:55:04 +0800

    Create setUpdata.js

---
+++ setUpdata.js
@@ -0,0 +1,21 @@
+import { setuppoints } from './setuppoints';
+
+export const setUpdata = (coordata ,immidata) => {
+const countrytable = new Map();
+  coordata.forEach((d, i) => {
+    d['points'] = [+d.longitude, +d.latitude];
+  });
+  coordata.forEach((d, i) => {
+    if (d.name == 'United States')
+      countrytable.set(
+        'United States of America',
+        d.points
+      );
+    else countrytable.set(d.name, d.points);
+  });
+  setuppoints(countrytable, immidata);
+    immidata.forEach( d =>{
+    if(typeof(d['2020']) === "string")
+      d['2020'] = +( (d['2020']).replace(/\s/g, '')) ;
+  })
+}
