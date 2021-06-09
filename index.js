d13b40ad912a7136a0db5ffafb98c5d2d22e9c21
serjunfan <80159533+serjunfan@users.noreply.github.com>
2021-06-09 15:54:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 //Main function. Actually a React H
-import React from 'react';
+import React, { useState ,useRef,useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import { useData } from './useData';
 import { Marks } from './Marks';
-import { immiData } from './immiData';
-import { getlalo } from './getlalo';
-import { setuppoints } from './setuppoints';
 
 const width = 960;
 const height = 500;
-//const projection = d3.geoNaturalEarth1();
 
 const App = () => {
-  const graphdata = useData();
-  const immidata = immiData();
-  if (!graphdata || !immidata ) {
-    return <pre>Loading...</pre>;
-  }
-  //console.log(graphdata);
-  const countrytable = new Map();
-  graphdata.countries.features.forEach( (d,i) =>{
-   countrytable.set(d.properties.name
-    ,(getlalo(d.geometry.coordinates, i)));
-  })
-  setuppoints(countrytable,immidata);
-  console.log(immidata);
-
+  const svgRef = useRef();
+  const gRef = useRef();
+  useEffect( () => {
+    const svg = d3.select(svgRef.current);
+    const g = d3.select(gRef.current);
+    const zoomFunc = d3.zoom().scaleExtent([0.8,5])
+    .translateExtent([[0,0],[width,height]])
+    .on("zoom", () =>{
+      g.attr('transform',d3.event.transform);
+      console.log("zoomed");
+      });
+    zoomFunc(svg);
+  } )
   return (
-    <svg width={width} height={height}>
-      <Marks graphdata={graphdata} immidata={immidata}/>
+    <svg width={width} height={height} ref={svgRef}
+      fill="current" stroke="current">
+      <g ref={gRef} >
+         <rect fill={"#110577"} width={width} height={height}/>
+          <Marks />
+      </g>
     </svg>
   );
 };
